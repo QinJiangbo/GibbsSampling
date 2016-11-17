cb1280b0c40443f4948268a466b5049df5e4bdcf
QinJiangbo <QinJiangbo@users.noreply.github.com>
2016-11-17 23:51:18 +0800

    Initial commit

---
+++ com/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+"""
+   Date: 17/11/2016
+   Author: qinjiangbo@github.io
+ 
+"""
---
+++ com/qinjiangbo/Documents.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+"""
+   Date: 17/11/2016
+   Author: qinjiangbo@github.io
+   Description:
+      Document class is mainly used to process the documents
+"""
+
+class Documents(object):
+    __good = None
+    def __init__(self, good):
+        __good = good
